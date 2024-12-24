9bf5328c940fb39d2aeebdb93f18c06d951eec32
David Cabaniuk <david.cabaniuk@cabsyware.com>
2024-12-24 21:01:23 +0000

    Example Singleton code

---
+++ Singleton/Logger.cs
@@ -0,0 +1,16 @@
+﻿namespace Singleton
+{
+    public class Logger
+    {
+        private static readonly Lazy<Logger> _instance = new(() => new Logger());
+
+        private Logger() { }
+
+        public static Logger Instance => _instance.Value;
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Log: {message}");
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,6 @@
+﻿using Singleton;
+
+Logger.Instance.Log("Application started.");
+Logger.Instance.Log("Processing data...");
+Logger.Instance.Log("Application ended.");
+
