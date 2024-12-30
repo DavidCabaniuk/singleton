137feeff6bc2e62875e3178c130af940e807d423
David Cabaniuk <david.cabaniuk@cabsyware.com>
2024-12-30 17:43:48 +0000

    Added comments

--- Singleton/Logger.cs
+++ Singleton/Logger.cs
@@ -2,12 +2,19 @@
 {
     public class Logger
     {
+        // Private static readonly instance, using Lazy<T> for lazy initialization
         private static readonly Lazy<Logger> _instance = new(() => new Logger());
 
-        private Logger() { }
+        // Private constructor to prevent instantiation from outside
+        private Logger() 
+        {
+            Console.WriteLine("Logger instance created.");
+        }
 
+        // Public property to access the instance
         public static Logger Instance => _instance.Value;
 
+        // Log method
         public void Log(string message)
         {
             Console.WriteLine($"Log: {message}");
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Singleton;
 
+// Client accessing the Singleton instance
 Logger.Instance.Log("Application started.");
 Logger.Instance.Log("Processing data...");
 Logger.Instance.Log("Application ended.");
